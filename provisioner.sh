6a3f2425519e0816187e50d0e54e616d1a9dbabc
William Dillon <william@housedillon.com>
2016-11-27 15:40:35 -0800

    Update to recent swift

--- provisioner.sh
+++ provisioner.sh
@@ -2,29 +2,29 @@
 
 echo --- initial setup
 sudo apt-get update
-sudo apt-get install -y android-tools-adb
-
-echo --- install NDK
-curl -LOs http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
-chmod a+x android-ndk-r10e-linux-x86_64.bin
-./android-ndk-r10e-linux-x86_64.bin > /dev/null
-export ANDROID_NDK_HOME=$HOME/android-ndk-r10e
-
-echo --- install SwiftAndroid
-curl -LOs https://github.com/SwiftAndroid/swift/releases/download/swiftandroid-2016-01-06/swift_android_2016-01-06.tar.xz
-tar xf swift_android_2016-01-06.tar.xz
-export PATH=$HOME/swiftandroid/bin:$PATH
-
-echo --- test compile
-echo 'print("Hello world!")' > hello.swift
-swiftc-android hello.swift
-file hello
+sudo apt-get install -y curl libcurl4-openssl-dev android-tools-adb
 
 echo --- install JDK
 sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install -y libncurses5:i386 libstdc++6:i386 zlib1g:i386
-sudo apt-get install -y openjdk-7-jdk
+sudo apt-get update 
+sudo apt-get install -y libncurses5:i386 libstdc++6:i386 zlib1g:i386 openjdk-7-jdk
+
+echo --- install NDK
+curl -LOs https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip
+unzip android-ndk-r13b-linux-x86_64.zip > /dev/null
+export ANDROID_NDK_HOME=$HOME/android-ndk-r13b
+
+echo --- install SwiftAndroid
+curl -LOs http://housedillon.com/other/SwiftAndroid.tar.gz
+tar xf SwiftAndroid.tar.gz
+export PATH=$HOME/usr/bin:$PATH
+
+echo --- Link android gold into /usr/bin
+sudo ln -s $ANDROID_NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin/ld.gold /usr/bin/armv7-none-linux-androideabi-ld.gold
+
+echo --- Hack around swiftc not finding the right linker \(temporary, in response to SR-1264\)
+sudo mv /usr/bin/ld.gold /usr/bin/ld.gold-orig
+sudo ln -s $ANDROID_NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/arm-linux-androideabi/bin/ld.gold /usr/bin/ld.gold
 
 echo --- install Android SDK
 curl -LOs http://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
@@ -35,7 +35,17 @@ echo "y" | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter platfor
 echo --- install Gradle
 sudo apt-get install -y git clang
 git clone https://github.com/SwiftAndroid/swift-android-gradle.git
-(cd swift-android-gradle && ./gradlew install)
+(cd swift-android-gradle && ./gradlew install > /dev/null)
+
+echo --- test compile
+echo 'print("Hello world!")' > hello.swift
+swiftc -v \
+    -target armv7-none-linux-androideabi \
+    -sdk $ANDROID_NDK_HOME/platforms/android-21/arch-arm \
+    -L   $ANDROID_NDK_HOME/sources/cxx-stl/llvm-libc++/libs/armeabi-v7a \
+    -L   ./android-ndk-r13b/toolchains/x86_64-4.9/prebuilt/linux-x86_64/lib/gcc/x86_64-linux-android/4.9.x/ \
+    hello.swift
+file hello
 
 echo --- compile Sample App
 git clone https://github.com/SwiftAndroid/swift-android-samples.git
